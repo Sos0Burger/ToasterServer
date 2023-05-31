b57f509ba58feadc1412f28678e9d28e231265f9
Ixor <dsv>
2023-05-31 14:41:51 +0300

    \

--- src/main/java/com/messenger/Messenger/config/WebSocketConfiguration.java
+++ src/main/java/com/messenger/Messenger/config/WebSocketConfiguration.java
@@ -15,6 +15,6 @@ public class WebSocketConfiguration implements WebSocketMessageBrokerConfigurer 
     }
     @Override
     public void registerStompEndpoints(StompEndpointRegistry registry) {
-        registry.addEndpoint("/stomp");
+        registry.addEndpoint("/stomp").withSockJS();
     }
 }
